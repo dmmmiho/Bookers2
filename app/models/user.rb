52b95596bfd9792234001b7119bce88da6b690ea
miho abe <106742176+dmmmiho@users.noreply.github.com>
2022-08-06 14:54:00 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_one_attached :profile_image
+  attachment :profile_image
   has_many :books, dependent: :destroy
+  
+  validates :name, presence: true,length: { minimum: 2, maximum: 20}
+  validates :introduction,length: { maximum: 50}
   
   def get_image
     unless image.attached?
